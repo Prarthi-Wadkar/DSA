5a2d78184fbea4f801d37cd0ef6239d70e361ed4
prarthi <prarthiwdkr@gmail.com>
2026-09-08 20:38:25 +0530

    arrays/hashing - two sum

---
+++ array/hashing/two_sum.py
@@ -0,0 +1,18 @@
+nums = [1,2,4,6]
+target = 10
+
+def two_sum(target, nums):
+    seen = {}
+
+    for i in range(len(nums)):
+        current = nums[i]
+        needed = target - needed 
+
+        if needed in seen:
+            return[current[needed], i]
+
+        seen[current] = i
+
+        
+
+    
