a49339537d35e2ffdff2564bb1b7c58e00a90ab5
prarthi <prarthiwdkr@gmail.com>
2026-09-09 18:16:08 +0530

    added anagram code

--- array/hashing/anagram.py
+++ array/hashing/anagram.py
@@ -1,7 +1,26 @@
-s = "prarthikartik"
-t = "kartikprarthi"
+s = "anagram"
+t = "angaram"
 
 def anagram(s,t):
     if len(s) != len(t):
         return False
-    
+    frequency_s = {}
+    frequency_t = {}
+
+    for character in s:
+        if character in frequency_s:
+            frequency_s[character] = frequency_s[character] + 1
+        else:
+            frequency_s[character] = 1
+
+    for characters in t:
+        if characters in frequency_t:
+            frequency_t[characters] = frequency_t[characters] + 1
+        else:
+            frequency_t[characters] = 1
+
+    for text in s:
+        print(frequency_s[character], character)
+
+
+anagram(s,t)
