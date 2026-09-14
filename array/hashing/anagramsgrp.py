8b0b2eed0f7a33a4e5dd86b726cb9c96d7a27798
prarthi <prarthiwdkr@gmail.com>
2026-09-14 19:20:44 +0530

    grp anagrams

---
+++ array/hashing/anagramsgrp.py
@@ -0,0 +1,17 @@
+strs = ["eat", "tea", "tan", "ate", "nat", "bat"]
+
+def group_anagrams(strs):
+    groups = {}
+
+    for word in strs:
+        key = ''.join(sorted(word))
+
+        if key not in groups:
+            groups[key] = []
+
+        groups[key].append(word)
+
+    return list(groups.values())
+
+
+print(group_anagrams(strs))
