0a6a3289a5f612ea2d36b9b11e76f99071c14425
prarthi <prarthiwdkr@gmail.com>
2026-09-17 13:34:57 +0530

    reversing of numbers learnt

---
+++ extraction_of_digits/reverse.py
@@ -0,0 +1,12 @@
+num = 234615
+n = num
+
+def reverse(n):
+    rev = 0
+    while n > 0:
+        digit = n % 10
+        rev = rev * 10 + digit 
+        n = n//10
+
+    return rev            
+print(reverse(n))
