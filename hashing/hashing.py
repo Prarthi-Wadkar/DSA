9c226d862144d3ee82ff318991c0e8ef0e8403af
prarthi <prarthiwdkr@gmail.com>
2026-09-24 19:04:51 +0530

    hashing day 2

---
+++ hashing/hashing.py
@@ -0,0 +1,14 @@
+#brute force method
+n = [5,3,2,2,1,5,5,7,5,10]
+m = [10,111,1,9,5,67,2]
+
+def frequency(n,m):
+    for num in m:
+        count = 0
+        for x in n:
+            if x == num:
+                count = +1
+
+    print(count)
+
+frequency(n,m)
