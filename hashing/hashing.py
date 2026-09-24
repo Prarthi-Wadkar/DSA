c4f57c3d9d3bd0e3fccae9c42302805663bd6e40
prarthi <prarthiwdkr@gmail.com>
2026-09-24 19:33:55 +0530

    optimal solution for hashing.py

--- hashing/hashing.py
+++ hashing/hashing.py
@@ -1,7 +1,8 @@
-#brute force method
 n = [5,3,2,2,1,5,5,7,5,10]
 m = [10,111,1,9,5,67,2]
 
+#brute force method
+"""
 def frequency(n,m):
     for num in m:
         count = 0
@@ -11,3 +12,16 @@ def frequency(n,m):
         print(count)
 
 frequency(n,m)
+"""
+
+#optimal solution
+freq = {}
+
+for i in n:
+    if i in freq:
+        freq[i] = +1
+    else:
+        freq[i] = 1
+
+for i in m:
+    print(freq.get(i, 0))
